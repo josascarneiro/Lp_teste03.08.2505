90483db599feced6e0dcb93ce7e8198261eb5790
josascarneiro <131828637+josascarneiro@users.noreply.github.com>
2025-08-03 22:58:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+<<<<<<< HEAD
+document.addEventListener('DOMContentLoaded', () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const utm = urlParams.get("utm_source") || "desconhecido";
+  document.getElementById('utm_source').value = utm;
+
+  document.getElementById('hotmartForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nome = this.nome.value;
+    const email = this.email.value;
+    const utm_source = this.utm_source.value;
+
+    const url = `https://script.google.com/macros/s/AKfycbwM4jvAq3wKooozqqjGZCZaTkvZTyMI4kKN3D9wTSw9KCWGIlKDednoT5UPP60OCa1a/exec?nome=${encodeURIComponent(nome)}&email=${encodeURIComponent(email)}&utm_source=${encodeURIComponent(utm_source)}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'sucesso') {
+          const urlHotmart = `https://go.hotmart.com/SEU_LINK_DE_COMPRA?name=${encodeURIComponent(nome)}&email=${encodeURIComponent(email)}&utm_source=${encodeURIComponent(utm_source)}`;
+          window.location.href = urlHotmart;
+        } else {
+          alert("Erro ao salvar os dados. Tente novamente.");
+        }
+      })
+      .catch(err => {
+        console.error('Erro:', err);
+        alert("Erro na conexão. Tente novamente.");
+      });
+  });
+=======
 document.addEventListener('DOMContentLoaded', () => {
   const urlParams = new URLSearchParams(window.location.search);
   const utm = urlParams.get("utm_source") || "desconhecido";
@@ -27,4 +58,5 @@ document.addEventListener('DOMContentLoaded', () => {
         alert("Erro na conexão. Tente novamente.");
       });
   });
+>>>>>>> 357781ebae2fdc069544981684fd7c7166a10e43
 });
